e09259ea204a1682151fadc7184848bd582d99b9
Md Hasanuzzaman <dr.has82@gmail.com>
2024-12-02 01:14:22 +0600

    ROLLER DICE DONE

--- Small/Roll Dice/main.py
+++ Small/Roll Dice/main.py
@@ -1,11 +1,17 @@
 from random import randint
+from collections import Counter
+
+print("Welcome to Dice Roll game! Input a number between 1 and 100. If you want to exit write \'exit\'")
 
 
 def roll_dice(amount: int = 2) -> list[int]:
     if amount <= 0:
         raise ValueError()
     rolls = [randint(1, 6) for _ in range(amount)]
-
+    counter_dict = Counter(rolls)
+    max_num = max(counter_dict, key=counter_dict.get)
+    counts = counter_dict[max_num]
+    print(f"{max_num} shows {counts} maximum times. So {max_num} is win!")
     return rolls
 
 
@@ -16,9 +22,10 @@ def main():
             if user_input.lower() == 'exit':
                 print("Thanks for playing")
                 break
-            print(roll_dice(int(user_input)))
+            print(*roll_dice(int(user_input)), sep=", ")
         except ValueError as e:
             print("Please enter a valid number")
 
 
-main()
+if __name__ == '__main__':
+    main()
